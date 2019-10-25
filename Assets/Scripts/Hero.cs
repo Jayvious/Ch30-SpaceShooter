a9df6c662f448217dbe2bcacdc5fe43780795663
Jayvious <54966469+Jayvious@users.noreply.github.com>
2019-10-24 22:30:34 -0400

    Last Minute

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-    public class Hero : MonoBehaviour
-    {
-        static public Hero S;
+public class Hero : MonoBehaviour
+   {
+        static public Hero S; //Singleton 
 
         [Header("Set In Inspector")]
         public float speed = 30;
